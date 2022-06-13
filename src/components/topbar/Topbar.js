0aee122c2846267c073d2ec2fd8d7e04ab7a78a7
shubhi182001 <kediashubhi@gmail.com>
2022-06-13 23:39:13 +0530

    started the project

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./topbar.css"
+
+function Topbar() {
+  return (
+    <div className='topbarContainer'>
+        <div className="topbarLeft">
+
+        </div>
+        <div className="topbarCenter">
+
+        </div>
+        <div className="topbarRight">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Topbar from '../../components/topbar/Topbar'
+import "./home.css"
+
+function Home() {
+  return (
+    <>
+    <Topbar/>
+    </>
+  )
+}
+
+export default Home
