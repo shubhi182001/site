ca410f61658e887c0f2b356eec77b95690523d09
shubhi182001 <kediashubhi@gmail.com>
2022-06-14 00:40:09 +0530

    sorted basic body components

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Feed from '../../components/feed/Feed'
+import Rightbar from '../../components/rightbar/Rightbar'
+import Sidebar from '../../components/sidebar/Sidebar'
 import Topbar from '../../components/topbar/Topbar'
 import "./home.css"
 
@@ -6,6 +9,12 @@ function Home() {
   return (
     <>
     <Topbar/>
+    <div className="homeContainer">
+        <Sidebar/>
+        <Feed/>
+        <Rightbar/>
+    </div>
+
     </>
   )
 }
