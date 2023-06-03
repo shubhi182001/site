02904c5eececab2693d7993156f0073d1703b5ee
shubhi182001 <kediashubhi@gmail.com>
2023-06-04 01:36:18 +0530

    login context api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { useContext } from "react";
 import Home from "./pages/home/Home";
 import Login from "./pages/Login/Login"
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const {user} = useContext(AuthContext);
+  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
+        <Route path="/" element={user ? <Home/>: <Register/>}/>
+        <Route path="/login" element={user? <Navigate replace to="/" /> :<Login/>}/>
+        <Route path="/register" element={user? <Navigate replace to="/" /> :<Register/>}/>
         <Route path="/profile/:username" element={<Profile/>}/>
 
       </Routes>
